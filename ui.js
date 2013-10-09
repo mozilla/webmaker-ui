c09f37b2a4075e7f50a34c57a17fab3c9d25feb0
Ali Al Dallal <ali@alicoding.com>
2013-10-09 18:14:29 -0400

    Both check

--- ui.js
+++ ui.js
@@ -21,13 +21,13 @@ define([ "text!./webmaker-ui-fragments.html" ], function(_fragments) {
           // `en`, `en-us`, `en-US` or `ady` 
           matches = href.match(/([a-z]{2,3})([-]([a-zA-Z]{2}))?/);
 
-          if (matches) {
-            if(matches[2]) {
-              matchesLang = matches[1].toLowerCase() + matches[2].toUpperCase();
-            } else {
-              matchesLang = matches[1].toLowerCase();
-            }
-          }
+      if (matches) {
+        if(matches[1] && matches[2]) {
+          matchesLang = matches[1].toLowerCase() + matches[2].toUpperCase();
+        } else {
+          matchesLang = matches[1].toLowerCase();
+        }
+      }
       // if the selected language is match to the language in the header
       if (selectedLang === lang) {
         return;
